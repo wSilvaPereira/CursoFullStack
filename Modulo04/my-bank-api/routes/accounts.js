16ae845e2c3fa9caffeea5c6f084bc5291a43394
William Pereira <william.pereira@escriba.com.br>
2020-10-14 10:33:32 -0300

    master

--- Modulo04/my-bank-api/routes/accounts.js
+++ Modulo04/my-bank-api/routes/accounts.js
@@ -43,4 +43,52 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+router.delete('/:id', async (req, res) => {
+  try {
+    const data = JSON.parse(await readFile(global.fileName));
+    const id = parseInt(req.params.id, 10);
+    data.accounts = data.accounts.filter((account) => {
+      return account.id !== id;
+    });
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
+    res.end();
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
+
+router.put('/', async (req, res) => {
+  try {
+    const data = JSON.parse(await readFile(global.fileName));
+    let account = req.body;
+    const id = parseInt(account.id, 10);
+    const index = data.accounts.findIndex((a) => a.id === id);
+
+    data.accounts[index] = account;
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
+    res.send(account);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
+
+router.patch('/updateBalance', async (req, res) => {
+  try {
+    let account = req.body;
+    const id = parseInt(account.id, 10);
+    const balance = parseFloat(account.balance);
+
+    const data = JSON.parse(await readFile(global.fileName));
+    const index = data.accounts.findIndex((a) => a.id === id);
+
+    data.accounts[index].balance = balance;
+
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
+
+    res.send(data.accounts[index]);
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
+
 export default router;
