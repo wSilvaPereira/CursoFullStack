94ac8ad5a1a4e96f460bd198e2da44fc2a3e2efc
William Pereira <william.pereira@escriba.com.br>
2020-10-19 08:38:49 -0300

    API

--- grades-control.api/index.js
+++ grades-control.api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { promises as fs } from 'fs';
 import gradeRouter from './routes/gradesRouter.js';
+import swaggerUI from 'swagger-ui-express';
+import { swaggerDocument } from './doc.js';
 
 const { readFile, writeFile } = fs;
 
@@ -8,6 +10,7 @@ global.fileName = 'grades.json';
 
 const app = express();
 app.use(express.json());
+app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use('/grade', gradeRouter);
 
